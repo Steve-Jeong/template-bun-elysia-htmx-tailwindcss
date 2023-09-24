eef1adebe190094bdc6f27d3aec19376071b506a
Steve Jeong <jst0930@gmail.com>
2023-09-24 11:13:00 +0900

    Add @elysiajs/html, HTML templete literal string

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ https://www.youtube.com/watch?v=cpzowDDJj24&t=345s
 1. bun init
 2. bun add elysia
 3. Make index.ts and run bun run --watch index.ts
+4. Add Elysis HTML plugin : bun add @elysiajs/html
+5. Import html from '@elysiajs/html' and add .use(html()) under Elysia object
+6. Add BastHtml literal string.
--- index.ts
+++ index.ts
@@ -1,9 +1,24 @@
 import {Elysia} from 'elysia'
+import {html} from '@elysiajs/html'
 
 const app = new Elysia()
-  .get('/', ()=> "Hello World!")
+  .use(html())
+  .get('/', ({html})=>html(BaseHtml))
   .listen(3001)
   
 console.log(
   `Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
 )
+
+const BaseHtml = `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>THE BETH STACK</title>
+</head>
+<body>I'm a html body</body>
+</html>
+`
