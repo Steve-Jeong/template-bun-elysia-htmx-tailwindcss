958908f93744bea475cfe0e66866c2945aa58b75
Steve Jeong <jst0930@gmail.com>
2023-09-24 10:59:57 +0900

    index.ts

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ https://www.youtube.com/watch?v=cpzowDDJj24&t=345s
 
 1. bun init
 2. bun add elysia
+3. Make index.ts and run bun run --watch index.ts
--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import {Elysia} from 'elysia'
+
+const app = new Elysia()
+  .get('/', ()=> "Hello World!")
+  .listen(3001)
+  
+console.log(
+  `Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
+)
